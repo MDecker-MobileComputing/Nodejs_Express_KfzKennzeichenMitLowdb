f03d4b41c2ca0cd48b1b043c62a09a8409ca3094
MDecker-MobileComputing <mide_42b@yahoo.de>
2024-02-26 16:55:57 +0100

    Doku-Block vor Middleware-Funktion verbessert

--- src/middleware.js
+++ src/middleware.js
@@ -2,13 +2,24 @@ import logging from "logging";
 
 const logger = logging.default("request");
 
-
 /**
  * Diese Middleware-Funktion schreibt für jeden HTTP-Request eine Zeile
- * auf den Logger.
+ * mit HTTP-Verb (z.B. `GET` oder `POST`) und Pfad (z.B. `/api/v1/...`
+ * auf den Logger `request`.
+ * <br><br>
+ *
+ * Beispiel-Zeile:
+ * ```
+ * GET /unterscheidungszeichen/v1/suchen/KA
+ * ```
+ *
+ * @param {*} req Request-Objekt
+ * @param {*} res Response-Objekt (wird nicht verwendet)
+ * @param {*} next Funktion, um nächste Middleware-Funktion aufzurufen
  */
 export function middlewareLogger(req, res, next) {
 
     logger.info(`${req.method} ${req.originalUrl}`);
+
     next();
 };
