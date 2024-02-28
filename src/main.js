8a2fd484f9e0e2948a40b9092fb73713f0e51629
MDecker-MobileComputing <mide_42b@yahoo.de>
2024-02-28 13:15:55 +0100

    Datei mit Controller umbenannt

--- src/main.js
+++ src/main.js
@@ -2,20 +2,20 @@ import express from "express";
 import logging from "logging";
 
 import {middlewareLogger}   from "./middleware/allgemein.middleware.js";
-import uzRoutenRegistrieren from "./unterscheidungszeichen.controller.js";
+import uzRoutenRegistrieren from "./uz.controller.js";
 
 
 const logger = logging.default("main");
 const app    = express();
 
 
-// HTTP konfigurieren
+// Express.js konfigurieren
 app.use( express.json() );
 app.use( express.static("public") );
-app.use(middlewareLogger);
-uzRoutenRegistrieren(app);
+app.use( middlewareLogger );
+uzRoutenRegistrieren( app );
 
-logger.info("Web-Server konfiguriert.\n");
+logger.info("Express.js (Web-Server) konfiguriert.\n");
 
 
 // Server starten
