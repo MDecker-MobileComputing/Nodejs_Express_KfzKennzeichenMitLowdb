93b3427f4fa0a233833cbf60b09367e92278ed85
MDecker-MobileComputing <mide_42b@yahoo.de>
2024-02-28 09:51:27 +0100

    Update unterscheidungszeichen.middleware.js

--- src/middleware/unterscheidungszeichen.middleware.js
+++ src/middleware/unterscheidungszeichen.middleware.js
@@ -1,19 +1,47 @@
+import { RestErgebnis }                 from '../model/RestErgebnis.model.js';
+import { UnterscheidungszeichenIntern } from '../model/UnterscheidungszeichenIntern.model.js';
+
 
 /**
  * Middleware-Funktionen für die Suche eines Unterscheidungszeichen.
  * Der Such-String (Pfad-Parameter) wird normalisiert, d.h. in Großbuchstaben
  * umgewandelt und führende und abschließende Leerzeichen entfernt.
  */
-function normalisiereSuchString(req, res, next) {
+function queryNormalisieren(req, res, next) {
 
-    const suchstring       = req.params.id;
-    const suchstringNormal = suchstring.toUpperCase().trim();
-    req.params.id          = suchstringNormal;
+    const suchString       = req.params.id;
+    const suchStringNormal = suchString.toUpperCase().trim();
+    req.params.id          = suchStringNormal;
 
-    next();
+    next(); // nächste Middleware-Funktion aufrufen
 }
 
-export const uzQueryMiddlewareArray = [ normalisiereSuchString  ];
+/**
+ * Middleware-Funktionen für die Suche eines Unterscheidungszeichen.
+ * Es wird überprüft, ob der Such-String aus 1 bis 3 Buchstaben besteht.
+ */
+function queryValidieren(req, res, next) {
+
+    const regex = /^[a-zA-Z]{1,3}$/; // regulärer Ausdruck
+
+    const suchString = req.params.id;
+
+    if ( regex.test(suchString) === false ) {
+
+        const ergebnisErfolglos = new RestErgebnis( false,
+            `Unterscheidungszeichen \"${suchString}\" besteht nicht aus 1 bis 3 Buchstaben.`,
+            new UnterscheidungszeichenIntern( "", "", "" )
+            );
+        res.send(ergebnisErfolglos)
+           .status(400); // Bad Request
+
+    } else {
+
+        next(); // nächste Middleware-Funktion aufrufen
+    }
+}
+
+export const uzQueryMiddlewareArray = [ queryNormalisieren, queryValidieren  ];
 
 
 /**
@@ -37,7 +65,7 @@ function postNormalisieren(req, res, next) {
         req.body.kategorie = req.body.kategorie.trim().toUpperCase();
     }
 
-    next();
+    next(); // nächste Middleware-Funktion aufrufen
 }
 
 
